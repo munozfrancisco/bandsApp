8a4a4a313193a6b3f36df3abe9e66b3f485ac705
Paco Padilla <padilla.fco@gmail.com>
2018-03-12 17:14:14 +0800

    added server file
    to run a local server

---
+++ server.js
@@ -0,0 +1,9 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8000, function(){
+    console.log('Server running on 8000...');
+});
+// cd navega hasta la carpeta 
+// npm install connect serve-static
+// pon este archivo en la raíz de la carpeta
+// ejecuta en consola node server.js
